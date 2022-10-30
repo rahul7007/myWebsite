9c0cdc4fbe8d49dee9385b795f83d942a54495e6
Rahul Sarma <rahulsarma7007@gmail.com>
2022-10-30 13:58:02 +0530

    Backend setup and API added for send email and OTP verification

---
+++ backend/src/server.ts
@@ -0,0 +1,73 @@
+import express from 'express';
+const bodyParser = require('body-parser')
+import cors from 'cors';
+
+const nodemailer = require('nodemailer');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(cors({
+  credentials: true,
+  origin: ["http://localhost:4200"]
+}))
+
+app.use(bodyParser.json())
+
+let otp: any;
+app.post('/api/generateAndSendOtp', async (req, res) => {
+
+  // generate OTP
+  otp = Math.floor(1000 + Math.random() * 9000);
+  console.log('otp', otp);
+
+  const customMessage = `Dear user, OTP to verify your email is: ${otp}. Please use it verify your email id`;
+
+  // create reusable transporter object using the default SMTP transport
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+      user: 'rahulsarma7007@gmail.com',
+      pass: 'hibvlyxzrzayqzfg'
+    },
+    tls: {
+      rejectUnauthorized: false
+    }
+  });
+
+  const mailOptions = {
+    from: 'noreply@portfolio.com',
+    to: 'rahulsarma9999@gmail.com',
+    subject: 'OTP-Verification',
+    text: customMessage
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, function (err: any, data: any) {
+    if (err) {
+      console.log('Something wrong (Send Email)', err);
+      res.json({ success: false, statusCode: 500, message: 'Error while sending email' });
+    } else {
+      console.log('Email sent !!!');
+      res.json({ success: true, statusCode: 200, message: 'Email sent !' });
+    }
+  });
+})
+
+app.post('/api/verifyOtp', async (req, res) => {
+  if (otp === parseInt(req.body.otp)) {
+    res.json({ success: true, statusCode: 200, message: 'OTP Verified !' })
+  } else {
+    res.json({ success: false, statusCode: 500, message: 'Incorrect OTP' })
+  }
+});
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log('Server is running on port', port);
+});
